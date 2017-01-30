b9c3bc3455c88d90bb9a6e96c9e4a786bce74a86
Rafael Ordoñez <rafaelordonezvivar@gmail.com>
2017-01-30 03:43:29 +0000

    Refactor to add colours to hours

--- src/main/java/rafael/ordonez/berlinclock/BerlinClock.java
+++ src/main/java/rafael/ordonez/berlinclock/BerlinClock.java
@@ -1,8 +1,12 @@
 package rafael.ordonez.berlinclock;
 
+import java.util.Arrays;
 import java.util.StringJoiner;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Created by rafa on 30/1/17.
@@ -10,19 +14,28 @@ import java.util.function.Function;
 public class BerlinClock {
 
     public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+    public static final int ROW_SIZE = 4;
 
-    private final TopLamp topLamp;
-    private final int fiveHourFieldsCount;
-    private final int fullHourFieldsCount;
+    private final Lamp topLamp;
+    private final Lamp [] fiveHourLamps;
+    private final Lamp [] fullHourLamps;
     private final int fiveMinuteFieldsCount;
     private final int fullMinuteFieldsCount;
 
     public BerlinClock(int hour, int minutes, int seconds) {
-        this.topLamp = TopLamp.values()[moduleOfTwo.apply(seconds)];
-        this.fiveHourFieldsCount = divideByFive.apply(hour);
-        this.fullHourFieldsCount = moduleOfFive.apply(hour);
+        this.topLamp = new Lamp(ColourEnum.YELLOW, StatusEnum.values()[moduleOfTwo.apply(seconds)]);
+        this.fiveHourLamps = fill(divideByFive.apply(hour));
+        this.fullHourLamps = fill(moduleOfFive.apply(hour));
         this.fiveMinuteFieldsCount = divideByFive.apply(minutes);
         this.fullMinuteFieldsCount = moduleOfFive.apply(minutes);
+    }
+
+    private Lamp[] fill(int lampsToTurnOn) {
+
+        return Stream.concat(
+                    IntStream.range(0, lampsToTurnOn).mapToObj(x -> new Lamp(ColourEnum.RED, StatusEnum.ON)),
+                    IntStream.range(0, ROW_SIZE - lampsToTurnOn).mapToObj(x1 -> new Lamp(ColourEnum.RED, StatusEnum.OFF)))
+                .toArray(size -> new Lamp[ROW_SIZE]);
     }
 
     BiFunction<Integer, Integer, Integer> module = (x, y) -> x % y;
@@ -34,15 +47,40 @@ public class BerlinClock {
     @Override
     public String toString() {
         return new StringJoiner(LINE_SEPARATOR)
-                .add(topLamp.name())
-                .add(String.valueOf(fiveHourFieldsCount))
-                .add(String.valueOf(fullHourFieldsCount))
+                .add(topLamp.toString())
+                .add(lampsIllumination(fiveHourLamps))
+                .add(lampsIllumination(fullHourLamps))
                 .add(String.valueOf(fiveMinuteFieldsCount))
                 .add(String.valueOf(fullMinuteFieldsCount))
                 .toString();
     }
+
+    private String lampsIllumination(Lamp [] lamps) {
+        return Arrays.stream(lamps)
+                .map(Lamp::toString)
+                .collect(Collectors.joining());
+    }
 }
 
-enum TopLamp {
+enum StatusEnum {
     ON, OFF
+}
+
+enum ColourEnum {
+    YELLOW, RED
+}
+
+class Lamp {
+    private final ColourEnum colour;
+    private final StatusEnum status;
+
+    public Lamp(ColourEnum colour, StatusEnum status) {
+        this.colour = colour;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + colour.name() + " " + status.name() + ")";
+    }
 }
--- src/test/java/rafael/ordonez/berlinclock/BerlinClockTest.java
+++ src/test/java/rafael/ordonez/berlinclock/BerlinClockTest.java
@@ -10,16 +10,16 @@ import static org.hamcrest.CoreMatchers.is;
  */
 public class BerlinClockTest {
 
-    public static final int SECOND_ZERO = 0;
-    private static final int ONE_SECOND = 1;
+    public static final int EVEN_SECOND = 0;
+    private static final int ODD_SECOND = 1;
 
     @Test
     public void topLampShouldBeOnWhenSecondsIsEven() throws Exception {
-        int seconds = SECOND_ZERO;
+        int seconds = EVEN_SECOND;
 
         BerlinClock berlinClock = new BerlinClock(0, 0, seconds);
 
-        Assert.assertThat("Top lamp should be ON", TopLamp.ON.name(), is(getLine(berlinClock.toString(), 1)));
+        Assert.assertThat("Top lamp should be ON", "(YELLOW ON)", is(getLine(berlinClock.toString(), 1)));
     }
 
     private static String getLine(String text, int line) {
@@ -28,37 +28,37 @@ public class BerlinClockTest {
 
     @Test
     public void topLampShouldBeOffWhenSecondsIsOdd() throws Exception {
-        int seconds = ONE_SECOND;
+        int seconds = ODD_SECOND;
 
         BerlinClock berlinClock = new BerlinClock(0, 0, seconds);
 
-        Assert.assertThat("Top lamp should be OFF", TopLamp.OFF.name(), is(getLine(berlinClock.toString(), 1)));
+        Assert.assertThat("Top lamp should be OFF", "(YELLOW OFF)", is(getLine(berlinClock.toString(), 1)));
     }
 
     @Test
     public void firstLineShouldDenoteFiveFullHourCounter() throws Exception {
         int hour = 10;
 
-        BerlinClock berlinClock = new BerlinClock(hour, 0, SECOND_ZERO);
+        BerlinClock berlinClock = new BerlinClock(hour, 0, EVEN_SECOND);
 
-        Assert.assertThat("First line Should be 2 for 10am", "2", is(getLine(berlinClock.toString(), 2)));
+        Assert.assertThat("First line Should be 2 for 10am", "(RED ON)(RED ON)(RED OFF)(RED OFF)", is(getLine(berlinClock.toString(), 2)));
     }
 
     @Test
     public void secondLineShouldDenoteFullHourFields() throws Exception {
         int hour = 22;
 
-        BerlinClock berlinClock = new BerlinClock(hour, 0, ONE_SECOND);
+        BerlinClock berlinClock = new BerlinClock(hour, 0, ODD_SECOND);
 
-        Assert.assertThat("First line should be 4 for 9pm", "4", is(getLine(berlinClock.toString(), 2)));
-        Assert.assertThat("Second line should be 2 for 9pm", "2", is(getLine(berlinClock.toString(), 3)));
+        Assert.assertThat("First line should be 4 for 9pm", "(RED ON)(RED ON)(RED ON)(RED ON)", is(getLine(berlinClock.toString(), 2)));
+        Assert.assertThat("Second line should be 2 for 9pm", "(RED ON)(RED ON)(RED OFF)(RED OFF)", is(getLine(berlinClock.toString(), 3)));
     }
 
     @Test
     public void thirdLineShouldDenoteFiveFullMinuteCounter() throws Exception {
         int minutes = 43;
 
-        BerlinClock berlinClock = new BerlinClock(0, minutes, SECOND_ZERO);
+        BerlinClock berlinClock = new BerlinClock(0, minutes, EVEN_SECOND);
 
         Assert.assertThat("Third line should be 8 for 43 seconds", "8", is(getLine(berlinClock.toString(), 4)));
     }
@@ -67,7 +67,7 @@ public class BerlinClockTest {
     public void fourthLineShouldDenoteFullMinuteFieldsCounter() throws Exception {
         int minutes = 43;
 
-        BerlinClock berlinClock = new BerlinClock(0, minutes, SECOND_ZERO);
+        BerlinClock berlinClock = new BerlinClock(0, minutes, EVEN_SECOND);
 
         Assert.assertThat("Third line should be 8 for 43 seconds", "8", is(getLine(berlinClock.toString(), 4)));
         Assert.assertThat("Fourth line should be 3 for 43 seconds", "3", is(getLine(berlinClock.toString(), 5)));
